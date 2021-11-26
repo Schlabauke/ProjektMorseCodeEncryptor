22f82ba1551f0a1f7d3c4cfb4add5cb1e316ea34
Schlabauke <annabelle.jeffre@gmail.com>
2021-11-26 14:50:03 +0100

    ProjektMorseCodeEncryptor

---
+++ assets/js/main.js
@@ -0,0 +1,58 @@
+console.log("Hutinni!")
+
+let textOutput = document.getElementById("output")
+
+let morseAlphabet = [
+    { letter: "1", morseCode: ".----" },
+    { letter: "2", morseCode: "..---" },
+    { letter: "3", morseCode: "...--" },
+    { letter: "4", morseCode: "....-" },
+    { letter: "5", morseCode: "....." },
+    { letter: "6", morseCode: "-...." },
+    { letter: "7", morseCode: "--..." },
+    { letter: "8", morseCode: "---.." },
+    { letter: "9", morseCode: "----." },
+    { letter: "0", morseCode: "-----" },
+    { letter: " ", morseCode: "    " },
+    { letter: "A", morseCode: ".-" },
+    { letter: "B", morseCode: "-..." },
+    { letter: "C", morseCode: "-.-." },
+    { letter: "D", morseCode: "-.." },
+    { letter: "E", morseCode: "." },
+    { letter: "F", morseCode: "..-." },
+    { letter: "G", morseCode: "--." },
+    { letter: "H", morseCode: "...." },
+    { letter: "I", morseCode: ".." },
+    { letter: "J", morseCode: ".---" },
+    { letter: "K", morseCode: "-.-" },
+    { letter: "L", morseCode: ".-.." },
+    { letter: "M", morseCode: "--" },
+    { letter: "N", morseCode: "-." },
+    { letter: "O", morseCode: "---" },
+    { letter: "P", morseCode: ".--." },
+    { letter: "Q", morseCode: "--.-" },
+    { letter: "R", morseCode: ".-." },
+    { letter: "S", morseCode: "..." },
+    { letter: "T", morseCode: "-" },
+    { letter: "U", morseCode: "..-" },
+    { letter: "V", morseCode: "...-" },
+    { letter: "W", morseCode: ".--" },
+    { letter: "X", morseCode: "-..-" },
+    { letter: "Y", morseCode: "-.--" },
+    { letter: "Z", morseCode: "--.." }
+];
+
+
+toMorseCode = () => {
+    let textInput = document.getElementById("textInput").value.toUpperCase().split("")
+    console.log(textInput)
+    let emptyString = ""
+    for (let l = 0; l < textInput.length; l++) {
+        for (let i = 0; i < morseAlphabet.length; i++) {
+            if (textInput[l] == morseAlphabet[i].letter) {
+                emptyString += `${morseAlphabet[i].morseCode}`
+            }
+        }
+    } textOutput.innerHTML = emptyString
+}
+toMorseCode()
